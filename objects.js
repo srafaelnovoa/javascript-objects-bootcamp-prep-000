f3fa624f295b30cf5d1cde4914632795d3e16c58
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-14 16:22:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = new Object({ Devo: 'Whip It'});
 function updatePlaylist(obj, artistName, songTitle) {
   return Object.assign({}, obj, {[artistName] : songTitle})
 }
+function removeFromPlaylist(obj, artistName) {
+  var playlist = Object.assign({}, obj)
+  
+}
